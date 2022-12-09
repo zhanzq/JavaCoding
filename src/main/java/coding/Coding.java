c9645ce110baf6f46f6c30409092b4b469a9935f
zhanzq <zhanzhiqiang09@126.com>
2022-12-09 18:39:03 +0800

    reduce unnecessary codes from Coding.java

--- src/main/java/coding/Coding.java
+++ src/main/java/coding/Coding.java
@@ -1,66 +1,10 @@
-import java.util.*;
+package coding;
 
+/**
+ * @author zhanzq
+ */
 public class Coding {
 
-    private static <T> void printList(ArrayList<T> lst){
-        int sz = lst.size();
-        if(sz == 0){
-            System.out.println("lst is empty!");
-            return ;
-        }else{
-            System.out.print("lst: [" + lst.get(0));
-        }
-        for(int i = 1; i < sz; i++){
-            System.out.print(", " + lst.get(i));
-        }
-        System.out.println("]");
-    }
-
-    private static <T> void printList(T[] lst){
-        int sz = lst.length;
-        if(sz == 0){
-            System.out.println("lst is empty!");
-            return ;
-        }else{
-            System.out.print("lst: [" + lst[0]);
-        }
-        for(int i = 1; i < sz; i++){
-            System.out.print(", " + lst[i]);
-        }
-        System.out.println("]");
-    }
-
-    private static <T> void printSet(Set<T> st){
-        int sz = st.size();
-        if(sz == 0){
-            System.out.println("set is empty!");
-            return;
-        }
-        Iterator<T> it = st.iterator();
-        System.out.print("st: {" + it.next());
-        for(int i = 1; i < sz; i++){
-            System.out.print(", " + it.next());
-        }
-        System.out.println("}");
-    }
-
-    private static <T1, T2> void printMap(Map<T1, T2> mp){
-        int sz = mp.size();
-        if(sz == 0){
-            System.out.println("map is empty!");
-            return;
-        }
-        Iterator<T1> keys = mp.keySet().iterator();
-        T1 key = keys.next();
-        T2 val = mp.get(key);
-        System.out.print("st: {" + key + ": " + val);
-        for(int i = 1; i < sz; i++){
-            key = keys.next();
-            val = mp.get(key);
-            System.out.print(", " + key + ": " + val);
-        }
-        System.out.println("}");
-    }
 
     public static void main(String[] args){
         System.out.println(convertToBase7(100));
